4e898a4c2522278bf9c80730ee213701374d6342
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2023-10-28 01:34:57 -0400

    01-galery
    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const cont = document.querySelector(".gallery");
+cont.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+cont.addEventListener("click", onGalleryItemClick);
 
-console.log(galleryItems);
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  const imgGallery = event.target;
+  const original = imgGallery.dataset.source;
+  const description = imgGallery.alt;
+
+  const instance = basicLightbox.create(
+    `<div>
+      <img  src="${original}" alt="${description}" width="1140" />
+    </div>`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", onEscPress);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscPress(clk) {
+    if (clk.key === "Escape") {
+      instance.close();
+    }
+  }
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
